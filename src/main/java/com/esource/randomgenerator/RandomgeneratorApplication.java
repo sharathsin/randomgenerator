403164935ed9e9cc6a71d04db247a25299d5b896
s.jillela <s.jillela@samung.com>
2022-12-05 10:11:57 +0530

    firstcommit

---
+++ src/main/java/com/esource/randomgenerator/RandomgeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.esource.randomgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomgeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomgeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/esource/randomgenerator/RandomgeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esource.randomgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomgeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
